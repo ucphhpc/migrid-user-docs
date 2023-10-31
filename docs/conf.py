7da1f5bc9457aa08823c7006188872a9430b4c45
Aputsiak Niels Janussen <aputtu@gmail.com>
2023-10-31 10:23:04 +0100

    Config change and adding custom.css for overwriting default CSS

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
+html_css_files = ["custom.css"]
 # Favicon for browsertab etcetera
 html_favicon = '_static/favicon.ico'
