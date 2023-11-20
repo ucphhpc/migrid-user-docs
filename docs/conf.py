60dec94e5ff844cfb142209f223545aaf88c0c0a
Sebastian Larsen Prehn <slp@di.ku.dk>
2023-11-20 15:13:47 +0100

    remove emoji support

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,6 @@ author = "ERDA/SIF Support Team"
 # ones.
 extensions = [
     "sphinx_rtd_theme",
-    'sphinxemoji.sphinxemoji',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
